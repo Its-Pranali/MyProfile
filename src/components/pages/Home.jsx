da4c9d7b41272b382f91f085686a1cade7372588
Its-Pranali <pranalinikam1000@gmail.com>
2026-07-18 22:24:24 +0530

    fixed mobile view

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -71,25 +71,25 @@ function Home() {
             <section className="counter-sec text-light py-5">
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-3">
+                        <div className="col-md-3 my-2">
                             <div className="counter-card text-center">
                                 <h2 className="counter-count">30+</h2>
                                 <p className="counter-text">Years Experience</p>
                             </div>
                         </div>
-                        <div className="col-md-3">
+                        <div className="col-md-3 my-2">
                             <div className="counter-card text-center">
                                 <h2 className="counter-count">10+</h2>
                                 <p className="counter-text">Websites Build</p>
                             </div>
                         </div>
-                        <div className="col-md-3">
+                        <div className="col-md-3 my-2">
                             <div className="counter-card text-center">
                                 <h2 className="counter-count">5+</h2>
                                 <p className="counter-text">Frontend Tools</p>
                             </div>
                         </div>
-                        <div className="col-md-3">
+                        <div className="col-md-3 my-2">
                             <div className="counter-card text-center">
                                 <h2 className="counter-count">100%</h2>
                                 <p className="counter-text">Responsive Designs</p>
@@ -109,7 +109,7 @@ function Home() {
                     </div>
 
                     <div className="row service-card align-items-center mb-4">
-                        <div className="col-md-4">
+                        <div className="col-md-4 my-2">
                             <div className="unit">
                                 <h2 className="exp-heading">UI Developer</h2>
                                 <p className="text">Nov 2024 – Present</p>
@@ -154,7 +154,7 @@ function Home() {
                     </div>
 
                     <div className="row service-card align-items-center mb-4">
-                        <div className="col-md-4">
+                        <div className="col-md-4 my-2">
                             <div className="unit">
                                 <h2 className="exp-heading">Jr. Web Developer</h2>
                                 <p className="text">Sept 2023 – Sept 2024</p>
@@ -365,7 +365,7 @@ function Home() {
                 <div className="container">
                     <div className="row">
                         <div className="col-md-12">
-                            <div className="d-flex gap-2 justify-content-center align-items-center mb-4">
+                            <div className="project-div d-flex justify-content-center align-items-center mb-4">
                                 <div className="skill-icon cms-icon">
                                     <svg aria-hidden="true" class="e-font-icon-svg e-fas-briefcase" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M320 336c0 8.84-7.16 16-16 16h-96c-8.84 0-16-7.16-16-16v-48H0v144c0 25.6 22.4 48 48 48h416c25.6 0 48-22.4 48-48V288H320v48zm144-208h-80V80c0-25.6-22.4-48-48-48H176c-25.6 0-48 22.4-48 48v48H48c-25.6 0-48 22.4-48 48v80h512v-80c0-25.6-22.4-48-48-48zm-144 0H192V96h128v32z"></path></svg>
                                 </div>
@@ -394,9 +394,9 @@ function Home() {
                                 <div class="project">
                                     <figure>
                                         <img decoding="async" src="assets/images/calculator-banner.png" alt="Thumb" />
-                                        <a href="https://bigbearvans.wuaze.com/">
-                                        </a><figcaption><a href="https://bigbearvans.wuaze.com/">
-                                        </a><a class="send-icon" href="https://bigbearvans.wuaze.com/" target="_blank">
+                                        <a href="https://pranali-scientific-calc.vercel.app/" target="_blank">
+                                        </a><figcaption><a href="https://pranali-scientific-calc.vercel.app/" target="_blank">
+                                        </a><a class="send-icon" href="https://pranali-scientific-calc.vercel.app/" target="_blank">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-external-link w-6 h-6 text-blue-400">
                                                     <path d="M15 3h6v6"></path>
                                                     <path d="M10 14 21 3"></path>
@@ -418,11 +418,11 @@ function Home() {
                                     <span class="gradient-tag">Git</span>
                                 </div>
 
-                                <div class="cons_w_wrapper project-view-btn d-flex justify-content-end gap-2 mt-2"> 
+                                <div class="cons_w_wrapper project-view-btn d-flex justify-content-end gap-2"> 
                                     <a href="https://github.com/Its-Pranali/Calculator" target="_blank" className="git-btn">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github w-5 h-5 text-gray-300 group-hover/btn:text-white relative z-10"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>
                                     </a>
-                                <a href="https://pranali-scientific-calc.vercel.app/" class="tmp-btn temp-btn hover-icon-reverse btn-border tmp-modern-button radius-round download-icon text-decoration-none" data-align="">
+                                <a href="https://pranali-scientific-calc.vercel.app/" target="_blank" class="tmp-btn temp-btn hover-icon-reverse btn-border tmp-modern-button radius-round download-icon text-decoration-none" data-align="">
                                     <span class="icon-reverse-wrapper">
                                         <span class="btn-text">Demo</span>
                                         <span class="btn-hack"></span>
@@ -463,8 +463,8 @@ function Home() {
                                     <figure>
                                         
                                          <img decoding="async" src="assets/images/fitness-banner.png" alt="Thumb" />
-                                        <a href="https://fitnessworld.wuaze.com/">
-                                        </a><figcaption><a href="https://fitnessworld.wuaze.com/">
+                                        <a href="https://fitnessworld.wuaze.com/" target="_blank">
+                                        </a><figcaption><a href="https://fitnessworld.wuaze.com/" target="_blank">
                                         </a><a class="send-icon" href="https://fitnessworld.wuaze.com/" target="_blank">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-external-link w-6 h-6 text-blue-400">
                                                     <path d="M15 3h6v6"></path>
@@ -490,7 +490,7 @@ function Home() {
                                     {/* <button className="git-btn">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github w-5 h-5 text-gray-300 group-hover/btn:text-white relative z-10"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>
                                     </button> */}
-                                <a href="https://fitnessworld.wuaze.com/" class="tmp-btn temp-btn hover-icon-reverse btn-border tmp-modern-button radius-round download-icon text-decoration-none" data-align="">
+                                <a href="https://fitnessworld.wuaze.com/" target="_blank" class="tmp-btn temp-btn hover-icon-reverse btn-border tmp-modern-button radius-round download-icon text-decoration-none" data-align="">
                                     <span class="icon-reverse-wrapper">
                                         <span class="btn-text">Demo</span>
                                         <span class="btn-hack"></span>
@@ -530,8 +530,8 @@ function Home() {
                                     <figure>
                                         
                                          <img decoding="async" src="assets/images/bbv-banner.png" alt="Thumb" />
-                                        <a href="https://bigbearvans.wuaze.com/">
-                                        </a><figcaption><a href="https://bigbearvans.wuaze.com/">
+                                        <a href="https://bigbearvans.wuaze.com/" target="_blank">
+                                        </a><figcaption><a href="https://bigbearvans.wuaze.com/" target="_blank">
                                         </a><a class="send-icon" href="https://bigbearvans.wuaze.com/" target="_blank">
                                                 <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-external-link w-6 h-6 text-blue-400">
                                                     <path d="M15 3h6v6"></path>
@@ -557,7 +557,7 @@ function Home() {
                                     {/* <button className="git-btn">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-github w-5 h-5 text-gray-300 group-hover/btn:text-white relative z-10"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path><path d="M9 18c-4.51 2-5-2-7-2"></path></svg>
                                     </button> */}
-                                <a href="https://bigbearvans.wuaze.com/" class="tmp-btn temp-btn hover-icon-reverse btn-border tmp-modern-button radius-round download-icon text-decoration-none" data-align="">
+                                <a href="https://bigbearvans.wuaze.com/" target="_blank" class="tmp-btn temp-btn hover-icon-reverse btn-border tmp-modern-button radius-round download-icon text-decoration-none" data-align="">
                                     <span class="icon-reverse-wrapper">
                                         <span class="btn-text">Demo</span>
                                         <span class="btn-hack"></span>
